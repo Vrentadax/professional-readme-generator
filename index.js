7c93d6f2a2d3ab059c7ca8c8450c29efd30322fe
Vrentadax <vrentadax@outlook.com>
2022-03-21 16:55:05 -0600

    begun seperating functions and fleshing out questions for user input

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-
-// TODO: Create an array of questions for user input
-const questions = [
-    //projectTitle
-    
-    //description
-
-    //table of contents
-
-    //installation
-
-    //usage
-
-    //license
-
-    //contributing
-
-    //tests
-    
-    //questions, includes github link/email
-
-];
+const fs = require('fs');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    ])
+};
 
 // Function call to initialize app
 init();
